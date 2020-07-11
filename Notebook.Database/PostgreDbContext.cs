adad0b9f004e71485a78b20d29c71b03d5e5f76a
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2020-07-11 23:27:50 +0200

    Connect with PostgreSQL Database

---
+++ Notebook.Database/PostgreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notebook.Domain.Entity;
+
+namespace Notebook.Database
+{
+    public class PostgreDbContext : DbContext
+    {
+        public PostgreDbContext(DbContextOptions<PostgreDbContext> options): base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
--- Notebook.WebClient/Startup.cs
+++ Notebook.WebClient/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Notebook.Database;
 
 namespace Notebook.WebClient
 {
@@ -19,6 +21,8 @@ namespace Notebook.WebClient
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddEntityFrameworkNpgsql().AddDbContext<PostgreDbContext>(opt =>  
+                opt.UseNpgsql(Configuration.GetConnectionString("NotebookConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
