e9d830020e3fbdcbd0e12d6b4d4b36a6543ece06
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2020-07-13 23:27:38 +0200

    Preparation for Home Page

---
+++ Notebook.WebClient/Controllers/ContactController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Notebook.WebClient.Interfaces;
+using Notebook.WebClient.Models;
+using System;
+using System.Linq;
+
+namespace Notebook.WebClient.Controllers
+{
+    public class ContactController : Controller
+    {
+        //private readonly ILogger<ContactController> _logger;
+        private readonly IContact _contactService;
+
+        public ContactController(/*ILogger<ContactController> logger,*/ IContact contactService)
+        {
+            //_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _contactService = contactService ?? throw new ArgumentNullException(nameof(contactService));
+        }
+
+        public IActionResult Index()
+        {
+            var contacts = _contactService.GetAllContacts();
+            var contactModel = contacts.Select(c => new ContactDetailModel
+            {
+                Id = c.Id,
+                FirstName = c.FirstName,
+                LastName = c.LastName ?? " "
+                
+            }).ToList();
+
+            var model = new ContactPortfolioListModel()
+            {
+                ContactPortfolioModels = contactModel
+            };
+
+            return View(model);
+        }
+
+
+        public IActionResult Detail(long contactId)
+        {
+            var contactRecord = _contactService.GetContactById(contactId);
+
+            var model = new ContactDetailModel
+            {
+                FirstName = contactRecord.FirstName,
+                LastName = contactRecord.LastName,
+                Patronymic = contactRecord.Patronymic,
+                BirthDate = contactRecord.BirthDate,
+                OrganizationName = contactRecord.OrganizationName,
+                Position = contactRecord.Position,
+                ContactInformations = contactRecord.CollectionInformations.Where(x => x.ContactId == contactId).ToList()
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new ContactCreateModel ();
+            return View(model);
+        }
+
+    }
+}
---
+++ Notebook.WebClient/Models/ContactDetailModel.cs
@@ -0,0 +1,27 @@
+﻿using Notebook.Domain.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace Notebook.WebClient.Models
+{
+    public class ContactDetailModel
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public string FullName => FirstName + " " + LastName;
+
+        public DateTime? BirthDate { get; set; }
+
+        public string OrganizationName { get; set; }
+
+        public string Position { get; set; }
+
+        public IEnumerable<ContactInformation> ContactInformations { get; set; }
+    }
+}
